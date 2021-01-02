156cf70384ab0b520fb3242016f1e55a8af71e18
Александр Марфицын <nonesasha225@gmail.com>
2021-01-03 01:30:46 +0500

    Добавьте файлы проекта.

---
+++ geekBrains_1.8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geekBrains_1._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            string activity;
+
+            Console.WriteLine(Properties.Settings.Default.greeting);
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.name))
+            {
+                Properties.Settings.Default.name = Console.ReadLine();
+                Properties.Settings.Default.age = Console.ReadLine();
+                Properties.Settings.Default.activity = Console.ReadLine();
+                Properties.Settings.Default.Save();
+
+            }
+            name = Properties.Settings.Default.name;
+            age = Properties.Settings.Default.age;
+            activity = Properties.Settings.Default.activity;
+
+            age = Properties.Settings.Default.age;
+            Console.WriteLine($"{name} {age} {activity}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ geekBrains_1.8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("geekBrains_1.8")]
+[assembly: AssemblyDescription("Домашняя работа № 8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("geekBrains_1.8")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7a2b349e-81f6-48aa-a33c-d86e840147dc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
